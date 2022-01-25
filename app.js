276f50905680d592379860713ffb4ab630475ff3
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-01-25 13:21:49 -0800

    moving all changes to dev branch

--- app.js
+++ app.js
@@ -65,5 +65,9 @@ function handleGuess(correctSpot, userGuess) {
     
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
+
+
+
+
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 
